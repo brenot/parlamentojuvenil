b1687a8bf74f94e6d1a15643aa23a58ac87db2f5
BMasquio <brunomasquio@gmail.com>
2020-03-05 16:16:54 -0300

    Cache bust

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir')
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +12,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss').webpack('app.js');
-});
+  mix.sass('app.scss').webpack('app.js')
+})
+
+elixir(function(mix) {
+  mix.version(['css/app.css', 'js/app.js'])
+})
