45b8070db971b27d530ae3fea9b53c51232c0b04
Breno Trengrouse <breno@gmail.com>
2020-03-06 14:15:26 -0300

    Responsive Adjusts BUBBLES 014

--- resources/views/2020/home/index.blade.php
+++ resources/views/2020/home/index.blade.php
@@ -5,11 +5,11 @@
     <div class="home-layout">
 
 
-        @include( get_current_year().'.home.partials.swiper')
-        @include( get_current_year().'.home.partials.video')
+{{--        @include( get_current_year().'.home.partials.swiper')--}}
+        {{--@include( get_current_year().'.home.partials.video')--}}
+        {{--@include( get_current_year().'.home.partials.howitworks')--}}
 
 
-        @include( get_current_year().'.home.partials.howitworks')
 
 
         {{--        @include( get_current_year().'.home.partials.about')--}}
@@ -17,7 +17,7 @@
 
 
 
-        @include( get_current_year().'.home.partials.whocan')
+        {{--@include( get_current_year().'.home.partials.whocan')--}}
 
 
 
@@ -30,7 +30,7 @@
         --}}
 
 
-            @include( get_current_year().'.home.partials.instagram')
+            {{--@include( get_current_year().'.home.partials.instagram')--}}
 
     </div>
 
