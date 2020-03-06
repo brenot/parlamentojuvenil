382f0728f3f448853a20948e49f736a41f012d55
Breno Trengrouse <breno@gmail.com>
2020-03-06 14:10:48 -0300

    Responsive Adjusts BUBBLES 013

--- resources/views/2020/home/partials/howitworks.blade.php
+++ resources/views/2020/home/partials/howitworks.blade.php
@@ -1,10 +1,10 @@
 
 <!-- COMO FUNCIONA Section -->
-<section class="comofunciona mb-0" id="como-funciona">
+<section class="comofunciona" id="como-funciona">
     <div class="container">
         <h2 class="text-center mt-5 mb-5">Como Funciona</h2>
         <div class="row">
-            <div class="col-12 pr-5 mt-5 ">
+            <div class="col-12">
                 {{--<h3 class="text-center ">Quem pode participar</h3>--}}
 
 
@@ -12,23 +12,42 @@
 
                     <blockquote class="electric bubble">
                         <div class="inner">
-                            <p>Cada município do Rio de Janeiro pode ser representado por um parlamentar juvenil.Exceto, a capital do Estado, que contará com três representantes e outros tês municípios com maior número de candidatos inscritos válidos, que terão dois PJ’s. <span>As eleições acontecem nas escolas estaduais, em dois turnos de votação.</span></p>
+                            <p>Cada município do Rio de Janeiro pode ser representado por um parlamentar juvenil.<br>
+                                <span class="small">Exceto, a capital do Estado, que contará com três representantes e outros três municípios com maior número de candidatos inscritos válidos, que terão dois PJ’s. </span></p>
+
                         </div>
                     </blockquote>
 
-                    <blockquote class="electric-reverse bubble">
+
+                    <blockquote class="speech bubble ">
+                        <div class="inner">
+                            <p>
+                                <span>As eleições acontecem nas escolas estaduais, em dois turnos de votação.</span>
+                            </p>
+                        </div>
+                    </blockquote>
+
+
+                    <blockquote class="electric bubble">
                         <div class="inner">
                             <p><span>Os Deputados eleitos aprendem mais sobre sua função em oficinas de capacitação preparadas pela Alerj.</span></p>
                         </div>
                     </blockquote>
 
-                    <blockquote class="electric bubble">
+                    <blockquote class="speech bubble">
                         <div class="inner">
-                            <p>Durante uma semana, os parlamentares apresentam, debatem e votam os projetos
-                                de sua autoria no plenário da Alerj.<br>
-                                <span>No fim, as escolhidas são encaminhadas à apreciação do Governador, podendo ou não virar Lei.</span></p>
+                            <p>
+                                Durante uma semana, os parlamentares apresentam, debatem e votam os projetos de sua autoria no plenário da Alerj.
+                            </p>
                         </div>
                     </blockquote>
+
+                    <blockquote class="electric bubble">
+                        <div class="inner">
+                            <p><span>No fim, as escolhidas são encaminhadas à apreciação do Governador, podendo ou não virar Lei.</span></p>
+                        </div>
+                    </blockquote>
+
 
                 </div>
 
