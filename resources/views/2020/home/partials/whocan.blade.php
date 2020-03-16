4f8cc015d5b995d319d0fefe5506627d68a16e4d
Breno Trengrouse <breno@gmail.com>
2020-03-16 13:03:17 -0300

    instagram padding top + link

--- resources/views/2020/home/partials/whocan.blade.php
+++ resources/views/2020/home/partials/whocan.blade.php
@@ -1,20 +1,9 @@
-
-
-
-
 <!-- SOBRE Section -->
 <section class="quempode " id="quem-pode">
-
     <div class="container">
         <div class="row text-center">
-
-
             <div class="col-12">
-
-
                 <h2 class="text-center mt-5 mb-5">Quem Pode Participar</h2>
-
-
                 <blockquote class="speech bubble mt-5">
                     <div class="inner">
                         <p>
@@ -38,59 +27,7 @@
                         </p>
                     </div>
                 </blockquote>
-
-
-
             </div>
-
-
-        </div>
-
-
-    </div>
-
-    {{--
-    <div class="container-fluid">
-        <!--<h2 class="text-center text-uppercase text-white">About</h2>-->
-        <div class="row">
-            <div class="offset-md-2 col-md-4 bg-green600 mr-2 p-3 text-center">
-                --}}{{--<h3 class="">O que é:</h3>--}}{{--
-
-
-                <div class="content">
-                    <p class="lead">O Parlamento Juvenil é um projeto da Assembleia Legislativa do Estado do Rio de Janeiro (Alerj) para incentivar a participação de jovens na política.</p>
-                    <p class="lead">Os estudantes passam uma semana vivendo o dia a dia de um Deputado Estadual, discutindo e aprimorando projetos de lei que eles mesmos criam. </p>
-                    <p class="lead">Os seis projetos aprovados ainda podem virar lei no Estado.</p>
-                </div>
-                <div class="img-mobile2017 "></div>
-
-            </div>
-            <div class="col-md-4 bg-green800 ml-2 p-3  text-center">
-                --}}{{--<h3 class="text-center ">Quem pode participar</h3>--}}{{--
-
-                <img class="img-fluid" src="/templates/{{ get_current_year() }}/images/logo-alerj-cores.png">
-                <p class="lead">
-                    O Parlamento Juvenil da Alerj acontece em parceria com a Secretaria de Estado de Educação e é multiplicado por toda a rede, pela Assembleia e pelos ex-parlamentares juvenis.
-                </p>
-                <p class="lead">
-                    Envolve as escolas estaduais dos 92 municípios do Estado do Rio, ou seja, mais de 600 mil pessoas.
-                </p>
-                <p class="lead">
-                    Desde a sua criação, foram parlamentares juvenis mais de 800 estudantes de todos os municípios do Estado.
-                </p>
-
-
-            </div>
-        </div>
-
-        <div class="text-center mt-4">
-            <a class="btn btn-xl btn-outline-light" href="#">
-                <i class="fas fa-download mr-2"></i>
-                Download Now!
-            </a>
         </div>
     </div>
-    --}}
-
-
 </section>
